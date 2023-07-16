d1ed57269e4daa6c82d9b132ec7adb50614d4ebb
MarkMeme <markmeme04@gmail.com>
2023-07-16 14:39:21 +0300

    post and get data

--- lib/screens/new_item_screen.dart
+++ lib/screens/new_item_screen.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:shopping_list_app/models/category.dart';
 import 'package:shopping_list_app/models/grocery_item.dart';
 import 'package:shopping_list_app/utils/categories.dart';
+import 'package:http/http.dart' as http;
 
 class NewItemScreen extends StatefulWidget {
   const NewItemScreen({super.key});
@@ -20,19 +22,31 @@ class _NewItemScreenState extends State<NewItemScreen> {
     var _enterdQunatity = 1;
     // ignore: no_leading_underscores_for_local_identifiers
     var _selectedCategory = categories[Categories.vegetables]!;
-
     // ignore: no_leading_underscores_for_local_identifiers
-    void _saveData() {
+    void _saveData() async {
       if (formKey.currentState!.validate()) {
         formKey.currentState!.save();
-        // print(_enterdQunatity);
-        // print(_selectedCategory);
-        // print(_enterdName);
+        final url = Uri.https('shopping-app-acb15-default-rtdb.firebaseio.com',
+            'shopping-list.json');
+        final response = await http.post(url,
+            headers: {'Content-Type': 'application/json'},
+            body: json.encode({
+              'name': _enterdName,
+              'quantity': _enterdQunatity,
+              'category': _selectedCategory.title,
+            }));
+
+        // ignore: use_build_context_synchronously
+        if (!context.mounted) {
+          return;
+        }
+        final Map<String, dynamic> resData = json.decode(response.body);
         Navigator.of(context).pop(GroceryItem(
-            category: _selectedCategory,
-            id: DateTime.now().toString(),
-            name: _enterdName,
-            quantity: _enterdQunatity));
+          id: resData['name'],
+          name: _enterdName,
+          quantity: _enterdQunatity,
+          category: _selectedCategory,
+        ));
       }
     }
 
